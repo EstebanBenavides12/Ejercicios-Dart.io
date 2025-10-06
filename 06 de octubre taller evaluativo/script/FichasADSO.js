9ef401d8f5649b60c720bdabd12fc897c7850dd9
EstebanBenavides12 <estebanbenavides1927@gmail.com>
2025-10-06 10:40:29 -0500

    Delete 06 de octubre taller evaluativo/script/FichasADSO.js

--- 06 de octubre taller evaluativo/script/FichasADSO.js
+++
@@ -1,6 +0,0 @@
-export async function obtenerFichas() {
-    const url = 'https://raw.githubusercontent.com/CesarMCuellarCha/apis/refs/heads/main/JUICIOS_ADSO.json';
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
